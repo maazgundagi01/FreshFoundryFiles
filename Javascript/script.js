91c4369f21681bfd2e0e120ce1bc425a3b29c45d
maazgundagi01 <mgundagi.01@gmail.com>
2021-11-27 20:56:21 -0600

    MAJOR UPDATE 1.4

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,23 @@
-var adminBtn = document.getElementById('admin-btn');
-adminBtn.style.display = "block";
+//var adminBtn = document.getElementById('admin-btn');
+//adminBtn.style.display = "block";
+document.addEventListener("DOMContentLoaded", function(){
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })
+});
+  console.log('abcd');
