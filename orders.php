6423642fedec4deee71b8644b26012207b705b15
ishand1993 <79570930+ishand1993@users.noreply.github.com>
2021-12-01 10:09:30 -0600

    Update orders.php

--- orders.php
+++ orders.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link rel="stylesheet" href="./Style/admin_style.css">
-    <link rel="stylesheet" href="./Style/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/e7083138fd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style/admin_style.css">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
 <header>
@@ -61,7 +61,7 @@
 
                 @include"db.php";
 
-                $cart_user = $_COOKIE["user_cookie"];
+                $cart_user = $_COOKIE["user_cookie"]."orders";
 
                 $query = "SELECT * FROM $cart_user";
                 $stmt = $db->prepare($query);
@@ -71,7 +71,7 @@
                 echo '<div class=\'order-page\'>';
                 echo '<p class=\'orders-head\'>My order</p><table>';
                 foreach($row as $r){
-                    echo '<tr><td><img style=\'width:200px;height:200px;object-fit:contain\' src=\'./uploads/'.$r['good_image'].'\'></td>';
+                    echo '<tr><td>'.$r['good_image'].'</td>';
                     echo '<td>'.$r['good_name'].'</td>';
                     echo '<td>'.$r['good_price'].'</td>';
                 };
