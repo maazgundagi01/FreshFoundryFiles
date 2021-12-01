f4049449a049a02d4f5fe849419497949f3bef54
maazgundagi01 <mgundagi.01@gmail.com>
2021-11-30 22:39:19 -0600

    nav un comment

--- users/chulbul.php
+++ users/chulbul.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/e7083138fd.js" crossorigin="anonymous"></script>
 </head>
-<body><!--
+<body>
 <header>
     <div class="logo">
         <a href="../index.php"><img src="../Assets/Images/FreshLogo0.3.png" alt=""></a>
@@ -31,7 +31,6 @@
             </ul>
         </nav>
     </div>
-</header>-->
 
 
 <?php
