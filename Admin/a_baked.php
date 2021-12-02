5b07125085fd3bc2f816a96630c0b88651f81eef
ishand1993 <79570930+ishand1993@users.noreply.github.com>
2021-12-01 21:24:41 -0600

    3.26

--- Admin/a_baked.php
+++ Admin/a_baked.php
@@ -85,7 +85,7 @@
         <div class="adminops">
             <!--Form Action => execute.php method POST-->
             <form action="goods_execute.php" method="POST" enctype="multipart/form-data">
-                <h1 class="formHead">Add Products</h1>
+                <h1 class="formHead">Add Products - Baked Goods</h1>
                 <!--Name Input-->
                 <div class="row">
                     <div class="mb-3 col">
@@ -110,7 +110,7 @@
                     </div>
                 <!--Product-->
                 <input  type="hidden" name="prod_name" id="" value="baked goods"/>
-                <div><button type="submit" class="submit" style="width:7rem; height:2rem;" >Submit</button><div>
+                <div><button type="submit" class="submit" style="border-radius:10px; border:none; box-shadow: 2px 2px 5px rgba(0,0,0,0.3); padding: 0.5em 1em;" >Add product</button><div>
                 </div>
             </form>
         </div>
@@ -123,9 +123,9 @@
                 $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 echo '<div style=\'margin-top:2em;text-align:center\'><form action=\'editprods.php\' method=\'POST\'><table>';
-                echo '<tr style=\'font-weight:bold;\'><td>Serial Number</td><td>Good Name</td><td>Good ID</td><td>Product ID</td><td>Prices</td><td>Image</td><td>Action</td></tr>';
+                echo '<tr style=\'font-weight:bold;\'><td>Good\'s Name</td><td>Good\'s ID</td><td>Product ID</td><td>Prices</td><td>Image</td><td>Action</td></tr>';
                 foreach($row as $r){
-                    echo '<tr><td>'.$r['sr_no'].'</td><input type=\'hidden\' name=\'goods_name\' value=\''.$r['goods'].'\'><td>'.$r['goods'].'</td><td>'.$r['goods_id'].'</td><input type=\'hidden\' name=\'prod_id\' value=\''.$r['prod_id'].'\'><td>'.$r['prod_id'].'</td><td>'.$r['price'].'</td><input type=\'hidden\' name=\'image\' value=\''.$r['image'].'\'><td><img style=\'width:100px;height:100px;object-fit:contain\' src=\'../uploads/'.$r['image'].'\'></td><td><input style=\'margin:1em;padding:.8em;border-radius:10px;border:none;box-shadow: 2px 2px 5px rgba(0,0,0,0.3);\' type=\'submit\' name=\'delete\' value=\'Delete entry\'></td></tr>';
+                    echo '<tr><input type=\'hidden\' name=\'goods_name\' value=\''.$r['goods'].'\'><td>'.$r['goods'].'</td><td>'.$r['goods_id'].'</td><input type=\'hidden\' name=\'prod_id\' value=\''.$r['prod_id'].'\'><td>'.$r['prod_id'].'</td><td>'.$r['price'].'</td><input type=\'hidden\' name=\'image\' value=\''.$r['image'].'\'><td><img style=\'width:100px;height:100px;object-fit:contain\' src=\'../uploads/'.$r['image'].'\'></td><td><input style=\'margin:1em;padding:.8em;border-radius:10px;border:none;box-shadow: 2px 2px 5px rgba(0,0,0,0.3);\' type=\'submit\' name=\'delete\' value=\'Delete entry\'></td></tr>';
                 }
                 echo '</table></form></div>';
             ?>
